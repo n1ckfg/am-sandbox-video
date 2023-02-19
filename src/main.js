4192b7d7638d54b9fb59bbe473544145682ce950
Nick Fox-Gieg <nick@fox-gieg.com>
2023-02-19 15:44:57 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,52 +1,52 @@
 function runGenerator(gen, resolve, reject, next, thrower, key, arg) {
-  let result, value;
-  try {
-    result = gen[key](arg);
-    value = result.value;
-  } catch (error) {
-    return void reject(error);
-  }
+    let result, value;
+    try {
+        result = gen[key](arg);
+        value = result.value;
+    } catch (error) {
+        return void reject(error);
+    }
 
-  if (result.done) {
-    resolve(value);
-  } else {
-    Promise.resolve(value).then(next, thrower);
-  }
+    if (result.done) {
+        resolve(value);
+    } else {
+        Promise.resolve(value).then(next, thrower);
+    }
 }
 
 (function() {
-  let generator = function*() {
-    let { sandbox: runGenerator } = yield import("./sandbox.js");
+    let generator = function*() {
+        let { sandbox: runGenerator } = yield import("./sandbox.js");
 
-    let location = window.location;
+        let location = window.location;
 
-    let url = location.hostname;
-    if (location.hostname === "localhost") {
-    	url = "http://" + url + ":" + location.port;
-    } else {
-		url = "https://" + url;
-    }
+        let url = location.hostname;
+        if (location.hostname === "localhost") {
+        	url = "http://" + url + ":" + location.port;
+        } else {
+		    url = "https://" + url;
+        }
 
-    if (location.pathname !== "/") {
-      let path = location.pathname.slice(1);
-      url = `${url}/${path}`;
-    }
+        if (location.pathname !== "/") {
+            let path = location.pathname.slice(1);
+            url = `${url}/${path}`;
+        }
 
-    yield runGenerator({ root: url, file: "artifact.js" });
-  };
+        yield runGenerator({ root: url, file: "artifact.js" });
+    };
 
-  return function() {
-    return new Promise((resolve, reject) => {
-      let generatorInstance = generator.apply(this, arguments);
-      function next(value) {
-        runGenerator(generatorInstance, resolve, reject, next, thrower, "next", value);
-      }
+    return function() {
+        return new Promise((resolve, reject) => {
+            let generatorInstance = generator.apply(this, arguments);
+            function next(value) {
+                runGenerator(generatorInstance, resolve, reject, next, thrower, "next", value);
+            }
 
-      function thrower(error) {
-        runGenerator(generatorInstance, resolve, reject, next, thrower, "throw", error);
-      }
+            function thrower(error) {
+                runGenerator(generatorInstance, resolve, reject, next, thrower, "throw", error);
+            }
 
-      next(void 0);
-    });
-  };
+            next(void 0);
+        });
+    };
 })()();
